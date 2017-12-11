9497b43745ad784bb05359712caf736f361a4835
Celeste De Santiago <celestedaphne@gmail.com>
2017-12-11 15:29:53 -0800

    Add views

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.get('/', function(req, res) {
-  res.end();
-});
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 const server = app.listen(3000, () => {
-  console.log('Listening on port %s...', server.address().port);
-});
+  console.log('Listening on port %s...', server.address().port)
+})
 
-module.exports = app;
+module.exports = app
