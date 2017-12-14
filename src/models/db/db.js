5f3275176ebc8eb0c3bc8e8d19b6936236cc242b
B Terbush <brittanyterbush@gmail.com>
2017-12-13 21:50:48 -0800

    Change file structure to reflect video by Punit.

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -1,7 +1,10 @@
 // Proper way to initialize and share the Database object
 // Loading and initializing the library:
 const pgp = require('pg-promise')()
+
+// Declaring connection details
 const connectionString = 'postgres://localhost:5432/bookstore_books'
+
 // Creating a new database instance from the connection details:
 const db = pgp(connectionString)
 
