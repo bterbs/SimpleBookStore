0a895ea22b5020a9306f48aa60dab8d0fa01823e
B Terbush <brittanyterbush@gmail.com>
2017-12-14 10:48:19 -0800

    add search box to homepage.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const pug = require('pug')
+const ejs = require('ejs')
 
 const app = express()
 
-app.use(express.static('view'))
+app.use(express.static('./src/view'))
+
+// set 'html' as the engine, using ejs's renderFile function
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
+
+app.get('/', (request, response) => {
+  response.render('.src/view/index')
+})
 
 const server = app.listen(3000, () => {
   console.log('Listening on port %s...', server.address().port)
