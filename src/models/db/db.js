1bcfad8a7884b4244cdaf58cf446ae23a55b2d8e
B Terbush <brittanyterbush@gmail.com>
2017-12-14 09:55:57 -0800

    reorganized code per curriculum.

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -3,6 +3,7 @@
 const pgp = require('pg-promise')()
 
 // Declaring connection details
+// `pg://${process.env.USER}@${process.env.LOCAL_HOST}/${process.env.DB_NAME}`
 const connectionString = 'postgres://localhost:5432/bookstore_books'
 
 // Creating a new database instance from the connection details:
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const pug = require('pug');
+const express = require('express')
+const pug = require('pug')
 
-const app = express();
+const app = express()
 
-app.use(express.static('view'));
+app.use(express.static('view'))
 
 const server = app.listen(3000, () => {
-  console.log('Listening on port %s...', server.address().port);
-});
+  console.log('Listening on port %s...', server.address().port)
+})
 
-module.exports = app;
+module.exports = app
